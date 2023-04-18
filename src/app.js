c24aa95fae2680656f7a9ce9f8651a14c02452ed
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-04-18 11:11:18 -0300

    litle changes on characters component

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import "./App.scss"
 import React, { Fragment } from "react";
 import Header from "./components/header";
 import Characters from "./components/characters";
+import { StrictMode } from "react";
 
 function App() {
     return (
         <Fragment>
             <Header title="Rick and Morty API"/>
-            <Characters />
+            <Characters/>
         </Fragment>
     );
 }
--- src/components/characters.js
+++ src/components/characters.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState, useEffect} from 'react'
 
 const Characters = () => {
 
@@ -6,30 +6,31 @@ const Characters = () => {
     const [character, setCharacter] = useState([])
 
     useEffect(() => {
+        
         fetch('https://rickandmortyapi.com/api/character')
             .then(response => response.json())
             .then(data => setCharacter(data.results))
+
     }, [])
 
     return (
         <div className="container">
             <div className="characters">
                 {
-                    character.map( item => {
+                    character.map( item => (
                         <div className="box" key={item.id}>
                             <img src={item.image} alt={item.name}/>
                             <div className="character">
                                 <h3 className="name">{item.name}</h3>
                                 <p className="specie">{item.species}</p>
                                 <p className="status">{item.status}</p>
-                                <p className="origin">{item.origin}</p>
                             </div>
                         </div>
-                    })
+                    ) )
                 }
             </div>
         </div>
-    );
+    )
 }
 
-export default Characters;
+export default Characters
