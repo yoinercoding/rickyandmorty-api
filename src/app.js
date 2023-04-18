48e4cf047fd52c74b74ba576ff5186a84f0d581b
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-04-18 10:49:09 -0300

    litles changes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import "./App.scss"
 import React, { Fragment } from "react";
 import Header from "./components/header";
+import Characters from "./components/characters";
 
 function App() {
     return (
         <Fragment>
-            <Header 
-                title="Rick and Morty API"
-            />
+            <Header title="Rick and Morty API"/>
+            <Characters />
         </Fragment>
     );
 }
