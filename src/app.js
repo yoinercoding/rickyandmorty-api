274771615af415312f66b50cc433ff67b3882c88
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-06-02 08:31:37 -0300

    sass -> css

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import "./App.scss"
 import React, { Fragment } from "react";
 import Header from "./components/header";
 import Characters from "./components/characters";
-import './main.sass'
+import './main.css'
 import './components/header.css'
 
 function App() {
